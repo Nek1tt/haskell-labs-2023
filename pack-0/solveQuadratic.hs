b193b583e5048d2855959100855ba57050e6e571
Nek1tt <144207428+Nek1tt@users.noreply.github.com>
2024-06-01 18:16:45 +0700

    Delete pack-0 directory

--- pack-0/solveQuadratic.hs
+++
@@ -1,5 +0,0 @@
-solveQuadratic :: Float -> Float -> Float -> (Float, Float)
-solveQuadratic a b c = if d < 0 then error "no solutions" else (x, y)
-    where d = b * b - 4 * a * c
-          x = (-b + sqrt d) / (2 * a)
-          y = (-b - sqrt d) / (2 * a)
